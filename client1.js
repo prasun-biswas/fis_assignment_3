51293a6f6fabafb6f2f56def9ef4653b482377ef
prasun-biswas <rpblikescodes@gmail.com>
2017-04-05 00:27:04 +0300

    add existing code

--- client1.js
+++ client1.js
@@ -1,4 +1,4 @@
-var express =require('express'); 
+var express =require('express');
 var app = require('express')();
 var bodyParser = require('body-parser');
 var http = require('http').Server(app);
@@ -9,6 +9,51 @@ var request = require('request');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+var Js2Xml = require("js2xml").Js2Xml;
+// var validator = require('xsd-schema-validator');
+
+app.post('/notifs', function(req, res){
+	console.log(req.body);
+	if(req.body.id == "Z2_Changed")
+	{
+		//cleint 1
+		request.post('http://localhost:4445',{form:{event:req.body}},
+			function(err,httpResponse,body){});
+		var MachineResponse = req.body;
+		var js2xml =new Js2Xml("MachineResponse", MachineResponse);
+		var MachineResponseXmlStr= js2xml.toString();
+		console.log(JSON.stringify(req.body.id));
+		console.log(MachineResponseXmlStr);
+		// var MachineResponseXsd="MachineResponseXsd.xsd";
+		// validator.validateXML(MachineResponseXmlStr,MachineResponseXsd,function (err,result) {
+		// 	if(err){ console.log('error found in validation process');
+		// 		console.log(err);
+		// 		process.exit(1);}
+		// 	console.log('xml file validated')
+		// })
+
+
+	}
+	else if(req.body.id == "Z3_Changed")
+	{
+
+		//cleint 1
+		request.post('http://localhost:4445',{form:{event:req.body}},
+			function(err,httpResponse,body){});
+
+		//cleint 2
+		request.post('http://localhost:4446',{form:{event:req.body}},
+			function(err,httpResponse,body){});
+	}
+	else if(req.body.id == "Z5_Changed")
+	{
+
+		//cleint 2
+		request.post('http://localhost:4446',{form:{event:req.body}},
+			function(err,httpResponse,body){});
+	}
+	res.end();
+});
 
 app.get('/sendMessage', function(req, res){
 	res.end();
--- testcode.js
+++ testcode.js
@@ -4,26 +4,32 @@
 
 var dateTime= new Date();
 console.log(dateTime);
-var name = "prasun";
-var newName = name + dateTime;
-console.log(newName);
-var soapMessage = "<?xml version="1.0" encoding="utf-8" ?>" +
+// var name = "prasun biswas is" +dateTime+
+//     "\n studing at TUT";
+// var newName = name + dateTime;
+// console.log(name);
 
-"<soap:Envelope xmlns:soap= "http://schemas.xmlsoap.org/soap/envelope/" +
-"xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" +
-"xmlns:xsd="http://www.w3.org/2001/XMLSchema>" +
-    "<soap:Header>"+
-"<IPC2501MsgInfo=MessageInfo"+
-"xmlns:IPC2501MsgInfo="http: www.webstds.ipc.org/2501/MessageInfo.xsd" +
-"sender:"+
-    "destination:"+
-        "dateTime:"+
-            "messageschema:"+
-                "messageId:"+
+// var soapMessage = '<?xml version=\"1.0\" encoding=\"utf-8\"?> \n\
+//  <soap:Envelope xmlns:soap= \"http://schemas.xmlsoap.org/soap/envelope/\"';
+// console.log(soapMessage);
 
-                    "</soap:Header>"+
-"<soap:Body>"+
-"</soap:Body>"+
-
-"</soap:Body>"+
-"</soap:Envelope>";
+//console.log(newName);
+var soapMessage = '<?xml version=\"1.0\" encoding=\"utf-8\" ?>'+
+    '\n<soap:Envelope xmlns:soap= \"http://schemas.xmlsoap.org/soap/envelope/\"'+
+'\nxmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" '+
+'\nxmlns:xsd=\"http://www.w3.org/2001/XMLSchema>\" '+
+   '\n<soap:Header>'+
+'\n<IPC2501MsgInfo=MessageInfo'+
+'\nxmlns:IPC2501MsgInfo=\"http: www.webstds.ipc.org/2501/MessageInfo.xsd\"'+
+'\nsender:'+
+'\ndestination:'+
+'\ndateTime: '+ dateTime+
+'\nmessageschema:'+
+'\nmessageId:'+
+'\n</soap:Header>'+
+'\n<soap:Body>'+
+'\n</soap:Body>'+
+'\n</soap:Envelope>';
+console.log(soapMessage);
+// var concatanate = newName +soapMessage;
+// console.log(concatanate);
